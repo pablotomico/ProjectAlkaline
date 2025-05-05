cad36a4a80a986a1855269384c4a0b632cfa3d80
John <John_Comrie@hotmail.co.uk>
2025-05-05 19:39:19 +0100

    Add DebugUI file which handles imgui buttons which call lambda functions

---
+++ src/Debug/DebugUI.cpp
@@ -0,0 +1,34 @@
+#ifdef DEBUG_BUILD
+
+#include "DebugUI.h"
+#include "rlImGui.h"
+#include "imgui.h"
+
+namespace alk
+{
+    namespace Debug
+    {
+        void UI::RegisterButton(const char* id, std::function<void()> callback) {
+            buttons.push_back({ id, callback });
+        }
+
+        void UI::Draw() {
+
+            rlImGuiBegin();
+            ImGui::Begin("Performance");
+            ImGui::Text("FPS: %d", GetFPS());
+
+            for (const auto& button : buttons) {
+                if (ImGui::Button(button.id)) {
+                    button.callback();
+                }
+            }
+
+            ImGui::End();
+            // end ImGui Content
+            rlImGuiEnd();
+        }
+    }
+}
+
+#endif // DEBUG_BUILD
--- src/systems/Scene.cpp
+++ src/systems/Scene.cpp
@@ -4,6 +4,7 @@
 #include "components/TransformComponent.h"
 #include "components/RenderComponent.h"
 #include "components/GridPreviewComponent.h"
+#include "Debug/DebugUI.h"
 
 namespace alk
 {
@@ -23,43 +24,7 @@ namespace alk
             world.GetComponent<TransformComponent>(gridRenderEntity)->GetPositionArray() = gridSystem->GetGridArray();
             alk::RenderSystem::AddToScreen(gridRenderEntity);
 
-            // army = world.CreateEntity();
-            // world.AddComponent<RenderComponent>(army, RenderSystem::RenderType::Sprite, "assets/sprites/test_unit.png");
-            // world.AddComponent<TransformComponent>(army, armySize);
-
-            // std::vector<Vector2> &positionArray = world.GetComponent<TransformComponent>(army)->GetPositionArray();
-            // for (int i = 0; i < armySize; ++i)
-            // {
-            //     int rx = std::rand() % 50;
-            //     int ry = std::rand() % 50;
-            //     Vector2 gridPosition = grid.GridToScreenPosition(Vector2{float(rx), float(ry)});
-
-            //     float dx = grid.GetTileWidthHalf() - (std::rand() % grid.GetTileWidth());
-            //     float dy = grid.GetTileHeightHalf() - (std::rand() % grid.GetTileHeight());
-
-            //     positionArray.emplace_back(Vector2{gridPosition.x - dx, gridPosition.y - dy});
-            // }
-            //alk::RenderSystem::AddToScreen(army);
-        }
-
-        void Scene::Update()
-        {
-            // if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-            // {
-            //     Vector2 worldPosition = GetScreenToWorld2D(GetMousePosition(), alk::RenderSystem::GetMainCamera());
-            //     auto gridPosition = GridHelpers::WorldToGridPosition(worldPosition);
-
-            //     if (gridPosition.x > -1)
-            //     {
-            //         ALK_LOG("Clicked in pos (%g, %g)", gridPosition.x, gridPosition.y);
-            //     }
-            //     else
-            //     {
-            //         ALK_LOG("Clicked outside of the grid");
-            //     }
-            // }
-
-            if (IsKeyPressed(KEY_SPACE)) {
+            alk::Debug::UI::RegisterButton("Spawn Castle", [this]() {
                 if(!world.IsValid(gridPlacementEntity))
                 {
                     gridPlacementEntity = world.CreateEntity();
@@ -68,9 +33,12 @@ namespace alk
                     world.AddComponent<GridPreviewComponent>(gridPlacementEntity, Vector2{3, 3});
                     alk::RenderSystem::AddToScreen(gridPlacementEntity);
                 }
-            }
+            });
+        }
 
-            if (IsKeyPressed(KEY_ENTER)) {
+        void Scene::Update()
+        {
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
                 if(world.IsValid(gridPlacementEntity))
                 {
                     Entity newBuilding = world.CreateEntity();
