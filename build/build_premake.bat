7c2445f3e02083cf2e0400e9af81beb61476c197
Pablo Tomico Pérez <pablotomico@hotmail.com>
2025-06-29 13:56:18 +0100

    Redone build and cleanbuild vscode tasks

--- build/build_premake.bat
+++ build/build_premake.bat
@@ -1,3 +1,38 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Default values
+set CONFIG=Release
+set DO_CLEAN=false
+
+REM Parse arguments
+:parse_args
+if "%~1"=="" goto end_args
+
+if "%~1"=="-d" (
+    set CONFIG=Debug
+)
+if "%~1"=="-c" (
+    set DO_CLEAN=true
+)
+
+shift
+goto parse_args
+
+:end_args
+
+echo Configuration: %CONFIG%
+echo Clean build: %DO_CLEAN%
+
+REM Run premake
 build\vendor\premake\premake5.exe vs2022 --file=premake5.lua
 
-msbuild ./build/projects/Alkaline.sln /m /p:Configuration=Debug
+REM Optionally clean
+if "%DO_CLEAN%"=="true" (
+    echo Running clean...
+    msbuild ./build/projects/Alkaline.sln /t:Clean /p:Configuration=%CONFIG%
+)
+
+REM Build
+echo Building...
+msbuild ./build/projects/Alkaline.sln /m /p:Configuration=%CONFIG%
