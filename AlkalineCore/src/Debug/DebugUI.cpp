a5d17d6f8e41e33321c5c19337c86e1e318d4a06
Pablo Tomico Pérez <pablotomico@hotmail.com>
2025-08-17 00:01:02 +0100

    Adding Game Logic Subsystem list as ImGui panel

--- AlkalineCore/src/Debug/DebugUI.cpp
+++ AlkalineCore/src/Debug/DebugUI.cpp
@@ -8,60 +8,83 @@
 
 #include "systems/ScriptSystem.h"
 #include "systems/GameLogic.h"
+#include "systems/subsystems/GameLogicSubsystem.h"
 
 namespace alk
 {
     namespace Debug
     {
-        void UI::RegisterText(const char *id, const char *text)
+        void UI::RegisterText(const char* id, const char* text)
         {
             textMap[id] = text;
         }
 
-        void UI::RegisterButton(const char* id, std::function<void()> callback) {
+        void UI::RegisterButton(const char* id, std::function<void()> callback)
+        {
             buttonMap[id] = { callback };
         }
 
-        void UI::UnregisterButton(const char *id)
+        void UI::UnregisterButton(const char* id)
         {
             auto it = buttonMap.find(id);
-            if (it != buttonMap.end()) {
+            if (it != buttonMap.end())
+            {
                 buttonMap.erase(it);
             }
         }
 
-        void UI::Draw() {
+        void UI::Draw()
+        {
 
             rlImGuiBegin();
 
             ImGui::Begin("Debug");
             ImGui::Text("FPS: %d", GetFPS());
-            
-            for (const auto& text : textMap) {
+
+            for (const auto& text : textMap)
+            {
                 ImGui::Text("%s: %s", text.first, text.second);
             }
             ImGui::Separator();
-            if (ImGui::Button("Run Lua Test")) {
+            if (ImGui::Button("Run Lua Test"))
+            {
                 alk::ScriptSystem::RunFile(std::string(GetWorkingDirectory()) + "/AlkalineGame/scripts/test.lua");
             }
 
-            if (ImGui::Button("Send TestNotification")) {
+            if (ImGui::Button("Send TestNotification"))
+            {
                 alk::ScriptSystem::SendNotification("TestNotification", 12, "param");
             }
-            
+
             ImGui::Separator();
             ImGui::Text("Buttons");
-            for (const auto& button : buttonMap) {
-                if (ImGui::Button(button.first)) {
+            for (const auto& button : buttonMap)
+            {
+                if (ImGui::Button(button.first))
+                {
                     button.second.callback();
                 }
             }
-            
+
             ImGui::End();
+
+            ImGui::Begin("Game Logic Subsystems");
+            for (auto system : alk::GameLogic::GetSubsystems())
+            {
+                if (system->IsEnabled())
+                {
+                    ImGui::TextColored(ImVec4(1,1,1,1), "%s", system->GetName().c_str());
+                }
+                else
+                {
+                    ImGui::TextColored(ImVec4(1,1,1,0.3f), "%s",system->GetName().c_str());
+                }
+            }
+            ImGui::End(); // Active Systems
 
             ImGui::Begin("World Outliner");
             World& world = alk::GameLogic::GetWorld();
-            for(auto& pair : world.GetAllEntities())
+            for (auto& pair : world.GetAllEntities())
             {
                 EntityMeta e = pair.second;
                 if (e.valid)
@@ -73,13 +96,14 @@ namespace alk
                     ImGui::TextDisabled(e.name.c_str());
                 }
             }
-            
+
             ImGui::Separator();
-            if (ImGui::Button("Test Serialize")) {
+            if (ImGui::Button("Test Serialize"))
+            {
                 sol::table table = alk::ScriptSystem::GetState().create_table();
                 alk::GameLogic::Scene* scene = alk::GameLogic::GetActiveScene();
                 alk::SceneSerializer::SerializeScene(*alk::GameLogic::GetActiveScene(), table);
-                
+
                 alk::ScriptSystem::SaveTableToFile("C:/dev/ProjectAlkaline/AlkalineGame/scenes/testexport.scene", table);
             }
 
